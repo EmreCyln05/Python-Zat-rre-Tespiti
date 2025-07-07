2dfcff9ac9bac452bb27a61c7693bc94e95f6ace
Emre CEYLAN <em3recyln@gmail.com>
2025-07-07 11:49:22 +0300

    Add files via upload

---
+++ import.py
@@ -0,0 +1,462 @@
+import tensorflow as tf
+import os
+import cv2
+from glob import glob
+import seaborn as sns
+import matplotlib.pyplot as plt
+import random
+import numpy as np
+
+from keras.preprocessing import image
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import RMSprop,Adam
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+print(tf.__version__) 
+
+BASE_DIR=('chest_xray')
+train_dir=os.path.join(BASE_DIR,'train/')
+val_dir=os.path.join(BASE_DIR,'val/')
+test_dir=os.path.join(BASE_DIR,'test/')
+
+print('Number of images in training set = ',str(len(glob(train_dir+'/'))))
+print('Number of images in validation set = ',str(len(glob(val_dir+'/'))))
+print('Number of images in testing set = ',str(len(glob(test_dir+'/'))))
+
+training_images = tf.io.gfile.glob('chest_xray/train//')
+validation_images = tf.io.gfile.glob('chest_xray/val//')
+
+
+total_files = training_images
+total_files.extend(validation_images)
+print(f'Total number of images : training_images + validation_images = {len(total_files)}\n')
+
+#spliting 80:20
+train_images, val_images = train_test_split(total_files, test_size = 0.2)
+print(f'After division of 80:20')
+print(f'Total number of training images = {len(train_images)}')
+print(f'Total number of validation images = {len(val_images)}')
+
+tf.io.gfile.makedirs('C:/Users/em3re/Desktop/proje/kaggle/working/val_dataset/NORMAL/')
+tf.io.gfile.makedirs('C:/Users/em3re/Desktop/proje/kaggle/working/val_dataset/PNEUMONIA/')
+tf.io.gfile.makedirs('C:/Users/em3re/Desktop/proje/kaggle/working/train_dataset/NORMAL/')
+tf.io.gfile.makedirs('C:/Users/em3re/Desktop/proje/kaggle/working/train_dataset/PNEUMONIA/')
+
+for ele in train_images:
+     parts_of_path = ele.split('\\')
+     # print(parts_of_path)
+
+     if 'PNEUMONIA' == parts_of_path[-2]:
+         tf.io.gfile.copy(src = ele, dst = 'C:/Users/em3re/Desktop/proje/kaggle/working/train_dataset/PNEUMONIA/' +  parts_of_path[-1])
+     else:
+         tf.io.gfile.copy(src = ele, dst = 'C:/Users/em3re/Desktop/proje/kaggle/working/train_dataset/NORMAL/' +  parts_of_path[-1])
+for ele in val_images:
+     parts_of_path = ele.split('\\')
+
+     if 'PNEUMONIA' == parts_of_path[-2]:
+         tf.io.gfile.copy(src = ele, dst = 'C:/Users/em3re/Desktop/proje/kaggle/working/val_dataset/PNEUMONIA/' +  parts_of_path[-1])
+     else:
+         tf.io.gfile.copy(src = ele, dst = 'C:/Users/em3re/Desktop/proje/kaggle/working/val_dataset/NORMAL/' +  parts_of_path[-1])
+
+print('Pneumonia x-ray images in training set after split = ',len(os.listdir('C:/Users/em3re/Desktop/proje/kaggle/working/train_dataset/PNEUMONIA/')))
+print('Normal x-ray images in training set after split = ',len(os.listdir('C:/Users/em3re/Desktop/proje/kaggle/working/train_dataset/NORMAL/')))
+print('Pneumonia x-ray images in validation set after split = ',len(os.listdir('C:/Users/em3re/Desktop/proje/kaggle/working/val_dataset/PNEUMONIA/')))
+print('Normal x-ray images in validation set after split = ',len(os.listdir('C:/Users/em3re/Desktop/proje/kaggle/working/val_dataset/NORMAL/')))
+print('Pneumonia x-ray images in test set = ',len(os.listdir('C:/Users/em3re/Desktop/proje/chest_xray/test/PNEUMONIA/')))
+print('Normal x-ray images in test set = ',len(os.listdir('C:/Users/em3re/Desktop/proje/chest_xray/test/NORMAL')))
+
+train_dir='C:/Users/em3re/Desktop/proje/kaggle/working/train_dataset/'
+val_dir='C:/Users/em3re/Desktop/proje/kaggle/working/val_dataset/'
+test_dir='C:/Users/em3re/Desktop/proje/chest_xray/test/'
+
+train_normal_dir='C:/Users/em3re/Desktop/proje/kaggle/working/train_dataset/NORMAL'
+train_pneumonia_dir='C:/Users/em3re/Desktop/proje/kaggle/working/train_dataset/PNEUMONIA'
+val_normal_dir='C:/Users/em3re/Desktop/proje/kaggle/working/val_dataset/NORMAL'
+val_pneumonia_dir='C:/Users/em3re/Desktop/proje/kaggle/working/val_dataset/PNEUMONIA'
+
+train_normal_fnames=os.listdir(train_normal_dir)
+train_pneumonia_fnames=os.listdir(train_pneumonia_dir)
+
+print(train_normal_fnames[:10])
+print(train_pneumonia_fnames[:10])
+
+
+####Görüntü
+# %matplotlib inline
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+no_cols=4
+no_rows=4
+
+pic_index=0
+
+fig=plt.gcf()
+fig.set_size_inches(no_cols*4,no_rows*4)
+
+pic_index+=8
+
+normal_pix=[os.path.join(train_normal_dir,fname) for fname in train_normal_fnames[pic_index-8:pic_index]]
+pneumonia_pix=[os.path.join(train_pneumonia_dir,fname) for fname in train_pneumonia_fnames[pic_index-8:pic_index]]
+
+for i,img_path in enumerate(normal_pix+pneumonia_pix):
+    sp=plt.subplot(no_rows,no_cols,i+1)
+    sp.axis()
+    
+    img=mpimg.imread(img_path)
+    plt.imshow(img,cmap='gray')
+    
+# plt.show()
+
+# first 8 images are normal x-ray images and next 8 images are pnemonia x-ray images
+
+model=tf.keras.models.Sequential([
+    # This is the first convolution
+    tf.keras.layers.Conv2D(16, (3,3), activation='relu',padding='same', input_shape=(180, 180, 1)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    # The second convolution
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu',padding='same'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The third convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu',padding='same'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The fourth convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu',padding='same'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The fifth convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu',padding='same'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # Flatten the results to feed into a DNN
+    tf.keras.layers.Flatten(),
+    # 512 neuron hidden layer
+    tf.keras.layers.Dense(512, activation='relu'),
+    # Only 1 output neuron. It will contain a value from 0-1 
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(optimizer=RMSprop(learning_rate=0.001),
+              loss='binary_crossentropy',
+              metrics=['accuracy','Precision','Recall'])
+model.summary()
+
+train_datagen=ImageDataGenerator(rescale=1.0/255,
+                                 rotation_range=30,
+                                 width_shift_range=0.2,
+                                 height_shift_range=0.2,
+                                 zoom_range=0.2,
+                                 )
+
+val_datagen=ImageDataGenerator(rescale=1.0/255)
+
+test_datagen=ImageDataGenerator(rescale=1.0/255)
+
+train_generator=train_datagen.flow_from_directory(train_dir,color_mode="grayscale",target_size=(180,180),batch_size=128,class_mode='binary')
+
+val_generator=val_datagen.flow_from_directory(val_dir,color_mode="grayscale",target_size=(180,180),batch_size=128,class_mode='binary')
+
+test_generator=test_datagen.flow_from_directory(test_dir,color_mode="grayscale",target_size=(180,180),batch_size=128,class_mode='binary')
+
+
+history=model.fit(train_generator,validation_data=val_generator,epochs=1,verbose=2)
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+train_precision=history.history['Precision']
+val_precision=history.history['val_Precision']
+
+train_recall=history.history['Recall']
+val_recall=history.history['val_Recall']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(acc))
+
+plt.plot(epochs, acc, 'r', label='Training accuracy')
+plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.figure()
+
+plt.plot(epochs, train_precision, 'r', label='Training precision')
+plt.plot(epochs, val_precision, 'b', label='Validation precision')
+plt.title('Training and validation precision')
+plt.legend()
+plt.figure()
+
+plt.plot(epochs, train_recall, 'r', label='Training recall')
+plt.plot(epochs, val_recall, 'b', label='Validation recall')
+plt.title('Training and validation recall')
+plt.legend()
+plt.figure()
+
+plt.plot(epochs, loss, 'r', label='Training Loss')
+plt.plot(epochs, val_loss, 'b', label='Validation Loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+# plt.show()
+
+eval_result1 = model.evaluate(test_generator, steps=624)
+print('loss  :', eval_result1[0])
+print('accuracy  :', eval_result1[1])
+print('Precision :', eval_result1[2])
+print('Recall :', eval_result1[3])
+
+# Tek bir görsel üzerinden Zatüre tespiti yapmak için
+
+# Görsel dosya yolunu belirtin
+img_path = 'C:/Users/em3re/Desktop/proje/person15_virus_46.jpeg'  # Görselin yolunu buraya yazın
+
+# Görseli yükleyin ve uygun boyutlara yeniden boyutlandırın
+img = image.load_img(img_path, target_size=(180, 180), color_mode='grayscale')
+# Görseli bir numpy dizisine dönüştürün
+img_array = image.img_to_array(img)
+
+# Görseli modelin beklediği giriş formatına uygun hale getirin
+img_array = np.expand_dims(img_array, axis=0)  # (1, 180, 180, 3)
+
+# Görseli normalize edin (0-1 aralığına getirin)
+img_array = img_array / 255.0
+
+# Model ile tahmin yapın
+prediction = model.predict(img_array)
+
+# Sonuç
+if prediction[0] > 0.5:
+    print("Zatüre tespiti: Zatüre")
+else:
+    print("Zatüre tespiti: Normal")
+
+# Görseli ve tahmini gösterelim
+plt.imshow(img)
+plt.title("Prediction: " + ("Pneumonia" if prediction[0] > 0.5 else "Normal"))
+plt.show()
+
+
+###Model 2
+train_datagen2=ImageDataGenerator(rescale=1.0/255,
+                                 rotation_range=30,
+                                 width_shift_range=0.2,
+                                 height_shift_range=0.2,
+                                 zoom_range=0.2,
+                                 )
+
+val_datagen2=ImageDataGenerator(rescale=1.0/255)
+
+test_datagen2=ImageDataGenerator(rescale=1.0/255)
+
+train_generator2=train_datagen2.flow_from_directory(train_dir,target_size=(180,180),batch_size=128,class_mode='binary')
+
+val_generator2=val_datagen2.flow_from_directory(val_dir,target_size=(180,180),batch_size=128,class_mode='binary')
+
+test_generator2=test_datagen2.flow_from_directory(test_dir,target_size=(180,180),batch_size=128,class_mode='binary')    
+
+###Importing and loading pretrained ResNet50 model
+from tensorflow.keras.applications.resnet50 import ResNet50
+pretrained_model2 = ResNet50(weights= 'imagenet', include_top=False, input_shape= (180,180,3))
+
+#freazing the trained layers
+for layers in pretrained_model2.layers:
+    layers.trainable = False
+#pretrained_model3.summary()
+
+last_layer=pretrained_model2.get_layer('conv5_block3_1_relu')
+last_output = last_layer.output
+
+x=tf.keras.layers.Flatten()(last_output)
+x=tf.keras.layers.Dense(1024,activation='relu')(x)
+x=tf.keras.layers.Dropout(0.2)(x)
+x=tf.keras.layers.Dense(256,activation='relu')(x)
+x=tf.keras.layers.Dropout(0.2)(x)
+x=tf.keras.layers.Dense(1,activation='sigmoid')(x)
+
+model2=tf.keras.Model(pretrained_model2.input,x)
+
+model2.compile(optimizer=RMSprop(learning_rate=0.001),
+              loss='binary_crossentropy',
+               metrics=['accuracy','Precision','Recall'])
+
+#model3.summary()
+
+history2=model2.fit(train_generator2,validation_data=val_generator2,epochs=1,verbose=2)
+
+acc2 = history2.history['accuracy']
+val_acc2 = history2.history['val_accuracy']
+
+train_precision2=history2.history['Precision']
+val_precision2=history2.history['val_Precision']
+
+train_recall2=history2.history['Recall']
+val_recall2=history2.history['val_Recall']
+
+loss2 = history2.history['loss']
+val_loss2 = history2.history['val_loss']
+epochs = range(len(acc2))
+
+plt.plot(epochs, acc2, 'r', label='Training accuracy')
+plt.plot(epochs, val_acc2, 'b', label='Validation accuracy')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.show()
+
+plt.plot(epochs, train_precision2, 'r', label='Training precision')
+plt.plot(epochs, val_precision2, 'b', label='Validation precision')
+plt.title('Training and validation precision')
+plt.legend()
+plt.show()
+
+plt.plot(epochs, train_recall2, 'r', label='Training recall')
+plt.plot(epochs, val_recall2, 'b', label='Validation recall')
+plt.title('Training and validation recall')
+plt.legend()
+plt.show()
+
+plt.plot(epochs, loss2, 'r', label='Training Loss')
+plt.plot(epochs, val_loss2, 'b', label='Validation Loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.show()
+
+# Tek bir görsel üzerinden Zatüre tespiti yapmak için
+
+# Görsel dosya yolunu belirtin
+img_path = 'C:/Users/em3re/Desktop/proje/person15_virus_46.jpeg'  # Görselin yolunu buraya yazın
+
+# Görseli yükleyin ve uygun boyutlara yeniden boyutlandırın
+img = image.load_img(img_path, target_size=(180, 180), color_mode='rgb')
+# Görseli bir numpy dizisine dönüştürün
+img_array = image.img_to_array(img)
+
+# Görseli modelin beklediği giriş formatına uygun hale getirin
+img_array = np.expand_dims(img_array, axis=0)  # (1, 180, 180, 3)
+
+# Görseli normalize edin (0-1 aralığına getirin)
+img_array = img_array / 255.0
+
+# Model ile tahmin yapın
+prediction = model2.predict(img_array)
+
+# Sonuç
+if prediction[0] > 0.5:
+    print("Zatüre tespiti: Zatüre")
+else:
+    print("Zatüre tespiti: Normal")
+
+# Görseli ve tahmini gösterelim
+plt.imshow(img)
+plt.title("Prediction: " + ("Pneumonia" if prediction[0] > 0.5 else "Normal"))
+plt.show()
+
+####Burada Kaldık !!! Denemedik !!!
+eval_result2 = model2.evaluate(test_generator2,steps=624)
+
+print('loss  :', eval_result2[0])
+print('accuracy  :', eval_result2[1])
+print('Precision :', eval_result2[2])
+print('Recall :', eval_result2[3])
+
+#Model3
+from tensorflow.keras.applications.inception_v3 import InceptionV3
+pretrained_model3=InceptionV3(input_shape=(180,180,3),
+                             include_top=False,
+                             weights='imagenet')
+#freazing the trained layers
+for layers in pretrained_model3.layers:
+    layers.trainable=False
+
+last_layer=pretrained_model3.get_layer('mixed10')
+last_output = last_layer.output
+
+x=tf.keras.layers.Flatten()(last_output)
+x=tf.keras.layers.Dense(1024,activation='relu')(x)
+x=tf.keras.layers.Dropout(0.2)(x)
+x=tf.keras.layers.Dense(256,activation='relu')(x)
+x=tf.keras.layers.Dropout(0.2)(x)
+x=tf.keras.layers.Dense(1,activation='sigmoid')(x)
+
+model3=tf.keras.Model(pretrained_model3.input,x)
+
+model3.compile(optimizer=RMSprop(learning_rate=0.001),
+              loss='binary_crossentropy',
+               metrics=['accuracy','Precision','Recall'])
+# model4.summary()
+history3=model3.fit(train_generator2,validation_data=val_generator2,epochs=1,verbose=2)
+
+acc3 = history3.history['accuracy']
+val_acc3 = history3.history['val_accuracy']
+
+train_precision3=history3.history['Precision']
+val_precision3=history3.history['val_Precision']
+
+train_recall3=history3.history['Recall']
+val_recall3=history3.history['val_Recall']
+
+loss3 = history3.history['loss']
+val_loss3 = history3.history['val_loss']
+epochs = range(len(acc3))
+
+
+plt.plot(epochs, acc3, 'r', label='Training accuracy')
+plt.plot(epochs, val_acc3, 'b', label='Validation accuracy')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.show()
+
+plt.plot(epochs, train_precision3, 'r', label='Training precision')
+plt.plot(epochs, val_precision3, 'b', label='Validation precision')
+plt.title('Training and validation precision')
+plt.legend()
+plt.show()
+
+plt.plot(epochs, train_recall3, 'r', label='Training recall')
+plt.plot(epochs, val_recall3, 'b', label='Validation recall')
+plt.title('Training and validation recall')
+plt.legend()
+plt.show()
+
+plt.plot(epochs, loss3, 'r', label='Training Loss')
+plt.plot(epochs, val_loss3, 'b', label='Validation Loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
+
+eval_result3 = model3.evaluate(test_generator2,steps=624)
+print('loss  :', eval_result3[0])
+print('accuracy  :', eval_result3[1])
+print('Precision :', eval_result3[2])
+print('Recall :', eval_result3[3])
+
+
+# Tek bir görsel üzerinden Zatüre tespiti yapmak için
+
+# Görsel dosya yolunu belirtin
+img_path = 'C:/Users/em3re/Desktop/proje/denepneumonia.jpg'  # Görselin yolunu buraya yazın
+
+# Görseli yükleyin ve uygun boyutlara yeniden boyutlandırın
+img = image.load_img(img_path, target_size=(180, 180), color_mode='rgb')
+# Görseli bir numpy dizisine dönüştürün
+img_array = image.img_to_array(img)
+
+# Görseli modelin beklediği giriş formatına uygun hale getirin
+img_array = np.expand_dims(img_array, axis=0)  # (1, 180, 180, 3)
+
+# Görseli normalize edin (0-1 aralığına getirin)
+img_array = img_array / 255.0
+
+# Model ile tahmin yapın
+prediction = model3.predict(img_array)
+
+# Sonuç
+if prediction[0] > 0.5:
+    print("Zatüre tespiti: Zatüre")
+else:
+    print("Zatüre tespiti: Normal")
+
+# Görseli ve tahmini gösterelim
+plt.imshow(img)
+plt.title("Prediction: " + ("Pneumonia" if prediction[0] > 0.5 else "Normal"))
+plt.show()
